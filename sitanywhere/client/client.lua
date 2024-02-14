4436a3f48214689363afb79fadc265356393e53d
yohananl <dartdartpart@gmail.com>
2024-02-13 20:34:35 -0800

    Draft for the sitanywhere resource

--- sitanywhere/client/client.lua
+++ sitanywhere/client/client.lua
@@ -12,7 +12,9 @@ local SitAnimations = {
     -- floor = { name = 'sit_phone_phonepickup_nowork', dictionary = 'anim@amb@business@bgen@bgen_no_work@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     floor = { name = 'owner_idle', dictionary = 'anim@heists@fleeca_bank@ig_7_jetski_owner', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     lean = { name = 'idle_a', dictionary = 'amb@world_human_leaning@male@wall@back@foot_up@idle_a', flag = AnimationFlags.ANIM_FLAG_REPEAT },
-    hang = { name = 'idle_a_jimmy', dictionary = 'timetable@jimmy@mics3_ig_15@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
+    platform = { name = 'idle_a_jimmy', dictionary = 'timetable@jimmy@mics3_ig_15@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
+    -- hang = { name = 'mics3_15_base_jimmy', dictionary = 'timetable@jimmy@mics3_ig_15@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
+    hang = { name = 'base', dictionary = 'amb@prop_human_seat_chair@male@elbows_on_knees@base', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     -- hang = { name = 'intro_loop_ped_a', dictionary = 'anim@heists@fleeca_bank@hostages@intro', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     -- hang = { name = 'base', dictionary = 'timetable@maid@couch@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     chair = { name = 'hanging_out_operator', dictionary = 'anim@amb@business@cfm@cfm_machine_no_work@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
@@ -25,19 +27,81 @@ local SitScenarios = {
     PROP_HUMAN_SEAT_CHAIR_FOOD = 'PROP_HUMAN_SEAT_CHAIR_FOOD',
     PROP_HUMAN_SEAT_CHAIR_MP_PLAYER = 'PROP_HUMAN_SEAT_CHAIR_MP_PLAYER',
 }
+
+local HeightLevels = {
+    Min = -1.5,
+    Ground = -0.9,
+    Max = 0.4,
+}
+
 --- ============================
 ---          Functions
 --- ============================
 
-function GetEntInFrontOfPlayer(Ped, Distance)
-    local Ent = nil
-    local CoA = GetEntityCoords(Ped, true)
-    local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, Distance, 0.0)
-    local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoA.x, CoA.y, CoA.z,
-        CoB.x, CoB.y, CoB.z - 0.5, -- Look down
-        16, Ped, 0)                -- 16 = Objects
-    local A, B, C, D, Ent = GetRaycastResult(RayHandle)
-    return Ent
+function GetEntInFrontOfPlayer(Ped)
+    color = { r = 0, g = 255, b = 0, a = 200 }
+
+    local forwardDistance = 0.5
+    local heightIndex = HeightLevels.Max
+    while heightIndex >= HeightLevels.Min do
+        local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, forwardDistance, heightIndex)
+        local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoB.x, CoB.y, CoB.z + 0.1,
+            CoB.x, CoB.y, CoB.z,
+            -1, Ped, 0) -- -1 = Everything
+        local shapeTestHandle, hit, endCoords, surfaceNormal, materialHash, entityHit =
+            GetShapeTestResultIncludingMaterial(RayHandle)
+
+        if hit == 1 then
+            return heightIndex, hit, endCoords, surfaceNormal, materialHash, entityHit
+        end
+
+        heightIndex = heightIndex - 0.1
+
+        Wait(1)
+    end
+
+    -- while true do
+    --     local heightIndex = 2
+    --     local CoA = GetEntityCoords(Ped, true)
+    --     local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, forwardDistance, HeightLevels.Ground)
+    --     local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoB.x, CoB.y, CoB.z + 0.1,
+    --         CoB.x, CoB.y, CoB.z,
+    --         -1, Ped, 0) -- -1 = Everything
+    --     local shapeTestHandle, hit, endCoords, surfaceNormal, materialHash, entityHit =
+    --         GetShapeTestResultIncludingMaterial(RayHandle)
+
+    --     DrawLine(CoB.x, CoB.y, CoB.z + 0.1, CoB.x, CoB.y, CoB.z, color.r, color.g, color.b,
+    --         color.a)
+    --     DrawMarker(28, CoB.x, CoB.y, CoB.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.1, 0.1, 0.1, color.r,
+    --         color.g, color.b, color.a, false, true, 2, nil, nil, false, false)
+
+    --     if IsControlJustReleased(0, 38) then
+    --         print(hit)
+    --         return heightIndex, hit, endCoords, surfaceNormal, materialHash, entityHit
+    --     end
+
+    --     Wait(1)
+    -- end
+
+    -- for heightIndex = #HeightLevels, 0, -1 do
+    --     local CoA = GetEntityCoords(Ped, true)
+    --     local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, forwardDistance, HeightLevels[heightIndex].level)
+    --     local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoA.x, CoA.y, CoA.z,
+    --         CoB.x, CoB.y, CoB.z,
+    --         -1, Ped, 0) -- -1 = Everything
+    --     local shapeTestHandle, hit, endCoords, surfaceNormal, materialHash, entityHit =
+    --         GetShapeTestResultIncludingMaterial(RayHandle)
+
+    --     -- If hit, get the results
+    --     if hit == 1 then
+    --         return heightIndex, hit, endCoords, surfaceNormal, materialHash, entityHit
+    --     end
+
+    --     -- If no hit and at lowest, return
+    --     if heightIndex == 1 then
+    --         return heightIndex, hit, endCoords, surfaceNormal, materialHash, entityHit
+    --     end
+    -- end
 end
 
 function requestAnimation(dictionary)
@@ -70,117 +134,113 @@ end
 --- ============================
 
 local isSitting = false
+
 RegisterCommand('sit', function()
     local playerPed = PlayerPedId()
 
     -- Check if already sitting, then cancel the animation
     if isSitting then
-        ClearPedTasks(playerPed)
+        ClearPedTasksImmediately(playerPed)
         isSitting = false
+
+        if IsEntityPositionFrozen(playerPed) then
+            FreezeEntityPosition(playerPed, false)
+        end
+
         return
     end
 
     -- Load all sit animations
     loadSitAnimations()
 
-    -- Stores the coordinates the ped will sit in
-    local x, y, z
-
     -- Get if there's object in front of the ped
-    local obj = GetEntInFrontOfPlayer(playerPed, 5.0)
+    local heightIndex, hit, endCoords, surfaceNormal, materialHash, entityHit = GetEntInFrontOfPlayer(playerPed)
 
     -- Get the current heading so the ped will turn around when sitting
     local heading = GetEntityHeading(playerPed)
 
-    -- If there's an object in front of the player
-    if obj ~= 0 then
-        -- Get the object hash
-        local objHash = GetEntityModel(obj)
-        -- Get the object max dimensions
-        local _, objMaxDimensions = GetModelDimensions(objHash)
-        -- Set the coordinates to sit on
-        x, y, z = table.unpack(GetEntityCoords(obj))
-        -- Add the object height to the z dimension
-        z = z + objMaxDimensions.z
+    print('heightIndex: ' .. tostring(heightIndex))
 
-        print('Object height: ' .. tostring(objMaxDimensions.z))
+    -- if heightIndex ~= 1 then
+    --     print('')
+    --     print('heightIndex: ' .. tostring(heightIndex))
+    --     print('hit: ' .. tostring(hit))
+    --     print('endCoords: ' .. tostring(endCoords.x) .. ', ' .. tostring(endCoords.y) .. ', ' .. tostring(endCoords.z))
+    --     print('surfaceNormal: ' .. tostring(surfaceNormal))
+    --     print('materialHash: ' .. tostring(materialHash))
+    --     print('entityHit: ' .. tostring(entityHit))
+    --     for key, value in pairs(MaterialHash) do
+    --         if materialHash == value then
+    --             print('MaterialType: ' .. tostring(key))
+    --         end
+    --     end
+    --     local entityType = 0
+    --     local entityModel = 0
+    --     entityType = GetEntityType(entityHit)
+    --     print('entityType: ' .. tostring(entityType))
+    --     if hit == 1 and entityType ~= 0 then
+    --         entityModel = GetEntityModel(entityHit)
+    --         print('entityModel: ' .. tostring(entityModel))
+    --     else
+    --         print('No entity model')
+    --     end
+    --     print('')
+    -- end
 
-        -- Check if object is low, sit on it like a chair
-        if objMaxDimensions.z < 1.0 then
-            -- Sit using scenario, reduce z and make ped turn around
-            TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
-                x, y, z - 0.5, heading + 180, 0, false, true)
-        elseif objMaxDimensions.z < 1.5 then -- Check if height is reachable enough to climb and sit on
-            -- Set the ped to the object coords with increased height
-            SetEntityCoords(playerPed, x, y, z, true, false, false, false)
-            -- Make the player turn around
-            SetEntityHeading(playerPed, heading + 180)
-            -- Sit while on top of the object
-            TaskPlayAnim(playerPed, SitAnimations.floor.dictionary, SitAnimations.floor.name,
-                8.0, 8.0, -1, SitAnimations.floor.flag, 0.0, false, false, false)
-        else -- Check if height is too high to sit on (e.g. post)
-            -- Make the player turn around
-            SetEntityHeading(playerPed, heading + 180)
-            -- Lean on the object
-            TaskPlayAnim(playerPed, SitAnimations.lean.dictionary, SitAnimations.lean.name,
-                8.0, 8.0, -1, SitAnimations.lean.flag, 0.0, false, false, false)
-        end
 
-        -- Set sitting to true
-        isSitting = true
 
-        -- Return and stop execution
-        return
-    end
 
-    -- No object, create an object in front of the player to get the ground coordinates
-    -- Get the player forward coords
-    local playerCoords = GetEntityCoords(playerPed)
-    local forward = GetEntityForwardVector(playerPed)
-    local forwardCoords = playerCoords + forward * 0.5
+    -- No ground in front, hang
+    if heightIndex == nil then
+        local playerCoords = GetEntityCoords(playerPed)
+        local forwardCoords = playerCoords + GetEntityForwardVector(playerPed) * 0.02
+        TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_CHAIR,
+            forwardCoords.x, forwardCoords.y, forwardCoords.z - 0.95, heading, 0, false, true)
 
-    -- Load object
-    local objHash = `prop_tool_blowtorch`
-    loadModel(objHash)
-    -- Create object in front of player with increased z dimension
-    obj = CreateObject(objHash, forwardCoords.x, forwardCoords.y, forwardCoords.z + 1.0, false, false, false)
-    -- Place the object on the ground
-    PlaceObjectOnGroundProperly(obj)
-    -- Get the object's coordinates
-    x, y, z = table.unpack(GetEntityCoords(obj))
-    -- Delete the object
-    DeleteEntity(obj)
-
-    -- print('Player coords: ' .. tostring(playerCoords.z))
-    -- print('Obj coords: ' .. tostring(z))
-
-    local zDifference = z - playerCoords.z
-    print('zDifference: ' .. tostring(zDifference))
-
-    -- Check if no ground in front
-    if zDifference <= -2.0 then
-        -- Sit in spot without moving and turning
-        TaskPlayAnim(playerPed, SitAnimations.hang.dictionary, SitAnimations.hang.name,
-            8.0, 8.0, -1, SitAnimations.hang.flag, 0.0, false, false, false)
-    elseif zDifference <= -0.55 then -- Check if only ground in front
-        -- Sit in spot without moving and turning
+        -- Freeze so ped doesn't fall
+        FreezeEntityPosition(playerPed, true)
+    elseif heightIndex <= HeightLevels.Ground then -- At ground, sit on floor
         TaskPlayAnim(playerPed, SitAnimations.floor.dictionary, SitAnimations.floor.name,
             8.0, 8.0, -1, SitAnimations.floor.flag, 0.0, false, false, false)
-    elseif zDifference < 0.1 then -- Check if a higher platform in ront
-        -- Set the ped to the object coords with increased height
-        SetEntityCoords(playerPed, x, y, z, true, false, false, false)
-        -- Make the player turn around
+    elseif heightIndex == HeightLevels.Max then -- Too high, lean
         SetEntityHeading(playerPed, heading + 180)
-        -- Sit while on top of platform
-        TaskPlayAnim(playerPed, SitAnimations.floor.dictionary, SitAnimations.floor.name,
-            8.0, 8.0, -1, SitAnimations.floor.flag, 0.0, false, false, false)
-    else -- Check if wall in front
-        -- Make the player turn around
-        SetEntityHeading(playerPed, heading + 180)
-        -- Lean on the object
         TaskPlayAnim(playerPed, SitAnimations.lean.dictionary, SitAnimations.lean.name,
             8.0, 8.0, -1, SitAnimations.lean.flag, 0.0, false, false, false)
+    else
+        --     local coords = endCoords + GetEntityForwardVector(entityHit) * 0.16
+        --     TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
+        --         coords.x, coords.y, coords.z + 0.03, heading + 180, 0, false, true)
     end
+
+    -- -- No ground, sit hang
+    -- if heightIndex == 1 then
+    --     local playerCoords = GetEntityCoords(playerPed)
+    --     local forwardCoords = playerCoords + GetEntityForwardVector(playerPed) * 0.02
+    --     TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_CHAIR,
+    --         forwardCoords.x, forwardCoords.y, forwardCoords.z - 0.95, heading, 0, false, true)
+
+    --     -- Freeze so ped doesn't fall
+    --     FreezeEntityPosition(playerPed, true)
+    -- elseif heightIndex == 2 then -- Ground, sit floor
+    --     TaskPlayAnim(playerPed, SitAnimations.floor.dictionary, SitAnimations.floor.name,
+    --         8.0, 8.0, -1, SitAnimations.floor.flag, 0.0, false, false, false)
+    -- elseif heightIndex == 3 then -- Low seat, sit chair
+    --     local coords = endCoords + GetEntityForwardVector(entityHit) * 0.16
+    --     TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
+    --         coords.x, coords.y, coords.z + 0.03, heading + 180, 0, false, true)
+    -- elseif heightIndex == 4 then -- Normal seat, sit chair
+    --     local coords = endCoords + GetEntityForwardVector(entityHit) * 0.175
+    --     TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
+    --         coords.x, coords.y, coords.z + 0.06, heading + 180, 0, false, true)
+    -- elseif heightIndex == 5 then -- High seat, sit platform
+    --     local coords = endCoords - GetEntityForwardVector(entityHit) * 0.45
+    --     TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
+    --         coords.x, coords.y, coords.z + 0.12, heading + 180, 0, false, true)
+    -- elseif heightIndex == #HeightLevels then -- Too high, lean
+    --     SetEntityHeading(playerPed, heading + 180)
+    --     TaskPlayAnim(playerPed, SitAnimations.lean.dictionary, SitAnimations.lean.name,
+    --         8.0, 8.0, -1, SitAnimations.lean.flag, 0.0, false, false, false)
+    -- end
 
     -- Set setting to true
     isSitting = true
