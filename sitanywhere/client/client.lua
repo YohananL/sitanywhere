db0399b2f499e2dac0891b76ed23275d0b4ce9b3
yohananl <dartdartpart@gmail.com>
2024-02-16 01:28:52 -0800

    Change ledge sit animation and add smoking command

--- sitanywhere/client/client.lua
+++ sitanywhere/client/client.lua
@@ -8,11 +8,11 @@ local AnimationFlags =
 };
 
 local SitAnimations = {
+    hang = { name = 'base', dictionary = 'amb@world_human_seat_wall@male@hands_by_sides@base', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     floor = { name = 'owner_idle', dictionary = 'anim@heists@fleeca_bank@ig_7_jetski_owner', flag = AnimationFlags.ANIM_FLAG_REPEAT },
-    lean = { name = 'idle_a', dictionary = 'amb@world_human_leaning@male@wall@back@foot_up@idle_a', flag = AnimationFlags.ANIM_FLAG_REPEAT },
-    platform = { name = 'idle_a_jimmy', dictionary = 'timetable@jimmy@mics3_ig_15@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
-    hang = { name = 'base', dictionary = 'amb@prop_human_seat_chair@male@elbows_on_knees@base', flag = AnimationFlags.ANIM_FLAG_REPEAT },
     chair = { name = 'hanging_out_operator', dictionary = 'anim@amb@business@cfm@cfm_machine_no_work@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
+    platform = { name = 'idle_a_jimmy', dictionary = 'timetable@jimmy@mics3_ig_15@', flag = AnimationFlags.ANIM_FLAG_REPEAT },
+    lean = { name = 'idle_a', dictionary = 'amb@world_human_leaning@male@wall@back@foot_up@idle_a', flag = AnimationFlags.ANIM_FLAG_REPEAT },
 }
 
 local SitScenarios = {
@@ -112,26 +112,6 @@ function GetDistanceFromEdge(Ped)
 
         Wait(1)
     end
-
-    -- while true do
-    --     local CoB = GetOffsetFromEntityInWorldCoords(Ped, 0.0, floorDistance, HeightLevels.Min)
-    --     local RayHandle = StartExpensiveSynchronousShapeTestLosProbe(CoB.x, CoB.y, CoB.z + zOffSet,
-    --         CoB.x, CoB.y, CoB.z, -1, Ped, 0) -- -1 = Everything
-
-    --     local _, hit, endCoords, _, _, _ = GetShapeTestResultIncludingMaterial(RayHandle)
-
-    --     DrawLine(CoB.x, CoB.y, CoB.z + zOffSet, CoB.x, CoB.y, CoB.z, color.r, color.g, color.b,
-    --         color.a)
-    --     DrawMarker(28, CoB.x, CoB.y, CoB.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.1, 0.1, 0.1, color.r,
-    --         color.g, color.b, color.a, false, true, 2, nil, nil, false, false)
-
-    --     if IsControlJustReleased(0, 38) then
-    --         print(hit)
-    --         return forwardDistance, hit, endCoords, _, _, _
-    --     end
-
-    --     Wait(1)
-    -- end
 end
 
 function requestAnimation(dictionary)
@@ -145,9 +125,15 @@ end
 
 function loadSitAnimations()
     for _, value in pairs(SitAnimations) do
-        if not (HasAnimDictLoaded(value.dictionary)) then
+        if not HasAnimDictLoaded(value.dictionary) then
             requestAnimation(value.dictionary)
         end
+    end
+end
+
+function unloadSitAnimations()
+    for _, value in pairs(SitAnimations) do
+        RemoveAnimDict(value.dictionary)
     end
 end
 
@@ -164,7 +150,7 @@ end
 
 local isSitting = false
 
-RegisterCommand('sit', function(_, args, _)
+RegisterCommand('sit', function()
     local playerPed = PlayerPedId()
 
     -- Check if already sitting, then cancel the animation
@@ -176,6 +162,9 @@ RegisterCommand('sit', function(_, args, _)
         if IsEntityPositionFrozen(playerPed) then
             FreezeEntityPosition(playerPed, false)
         end
+
+        -- Unload all sit animations
+        unloadSitAnimations()
 
         -- Reset isSitting property
         isSitting = false
@@ -202,19 +191,21 @@ RegisterCommand('sit', function(_, args, _)
 
         local forwardMultiplier
         if floorDistance >= -0.01 and floorDistance <= 0.01 then
-            forwardMultiplier = -0.25
+            forwardMultiplier = -0.17
         elseif floorDistance >= 0.09 and floorDistance <= 0.11 then
-            forwardMultiplier = -0.12
+            forwardMultiplier = -0.07
         elseif floorDistance >= 0.19 and floorDistance <= 0.21 then
-            forwardMultiplier = -0.05
+            forwardMultiplier = 0.00
         elseif floorDistance >= 0.29 and floorDistance <= 0.31 then
-            forwardMultiplier = 0.05
+            forwardMultiplier = 0.10
         elseif floorDistance >= 0.39 and floorDistance <= 0.41 then
-            forwardMultiplier = 0.1
+            forwardMultiplier = 0.18
         elseif floorDistance >= 0.49 and floorDistance <= 0.51 then
-            forwardMultiplier = 0.27
+            forwardMultiplier = 0.29
         elseif floorDistance >= 0.59 and floorDistance <= 0.61 then
-            forwardMultiplier = 0.38
+            forwardMultiplier = 0.41
+        elseif floorDistance >= 0.69 and floorDistance <= 0.71 then
+            forwardMultiplier = 0.45
         end
 
         print('forwardMultiplier: ' .. tostring(forwardMultiplier))
@@ -224,8 +215,13 @@ RegisterCommand('sit', function(_, args, _)
         local forwardCoords = playerCoords + GetEntityForwardVector(playerPed) * forwardMultiplier
 
         -- Sit from ledge
-        TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_CHAIR,
-            forwardCoords.x, forwardCoords.y, forwardCoords.z - 0.94, heading, 0, false, true)
+        TaskPlayAnimAdvanced(playerPed, SitAnimations.hang.dictionary, SitAnimations.hang.name,
+            forwardCoords.x, forwardCoords.y, forwardCoords.z - 0.65,
+            0.0, 0.0, GetEntityHeading(playerPed),
+            8.0, 8.0, -1, SitAnimations.hang.flag, 0.0, false, false, false)
+
+        -- Freeze so ped doesn't fall
+        FreezeEntityPosition(playerPed, true)
     elseif heightIndex <= HeightLevels.Ground then
         -- At ground, sit on floor
         TaskPlayAnim(playerPed, SitAnimations.floor.dictionary, SitAnimations.floor.name,
@@ -236,69 +232,147 @@ RegisterCommand('sit', function(_, args, _)
         TaskPlayAnim(playerPed, SitAnimations.lean.dictionary, SitAnimations.lean.name,
             8.0, 8.0, -1, SitAnimations.lean.flag, 0.0, false, false, false)
     else
-        -- print('')
-        -- print('hit: ' .. tostring(hit))
-        -- print('endCoords: ' ..
-        --     tostring(endCoords.x) .. ', ' .. tostring(endCoords.y) .. ', ' .. tostring(endCoords.z))
-        -- print('surfaceNormal: ' .. tostring(surfaceNormal))
-        -- print('materialHash: ' .. tostring(materialHash))
-        -- print('entityHit: ' .. tostring(entityHit))
-        -- for key, value in pairs(MaterialHash) do
-        --     if materialHash == value then
-        --         print('MaterialType: ' .. tostring(key))
-        --     end
-        -- end
-        -- local entityType = 0
-        -- local entityModel = 0
-        -- entityType = GetEntityType(entityHit)
-        -- print('entityType: ' .. tostring(entityType))
-        -- if hit == 1 and entityType ~= 0 then
-        --     entityModel = GetEntityModel(entityHit)
-        --     print('entityModel: ' .. tostring(entityModel))
-        -- else
-        --     print('No entity model')
-        -- end
-        -- print('')
-
-        local zOffset = 0.03
-
-
-        -- Sit normally, check if chair is specified
-        if GetEntityType(entityHit) ~= 0 and args[1] ~= nil then
-            -- Get the heading of the chair entity
-            heading = GetEntityHeading(entityHit)
-
-            -- If chair, get the coords of the chair
-            if args[1] == 'chair' then
-                -- Set the end coords to the coords of the entity
-                endCoords = GetEntityCoords(entityHit)
-
-                -- Sit further back in the chair
-                endCoords = endCoords + GetEntityForwardVector(entityHit) * 0.1
-
-                -- Use the zOffset for chairs
-                zOffset = 0.0
-            end
-
-            -- If bench,
-            if args[1] == 'bench' then
-                -- Sit further in front of the bench
-                endCoords = endCoords + GetEntityForwardVector(entityHit) * -0.11
-            end
-        end
-
         -- Make ped sit in the opposite direction
         heading = heading + 180
 
+        -- Sit down
         TaskStartScenarioAtPosition(playerPed, SitScenarios.PROP_HUMAN_SEAT_BENCH,
-            endCoords.x, endCoords.y, endCoords.z + zOffset, heading, 0, false, true)
+            endCoords.x, endCoords.y, endCoords.z + 0.03, heading, 0, false, true)
     end
-
-    -- -- Freeze so ped doesn't fall
-    -- FreezeEntityPosition(playerPed, true)
 
     -- Set setting to true
     isSitting = true
+end, false)
+
+--- ============================
+---           Smoking
+--- ============================
+
+RegisterCommand('smokeInPlace', function()
+    local playerPed = PlayerPedId()
+    local smokeScenario = 'WORLD_HUMAN_SMOKING_CLUBHOUSE'
+
+    if IsPedUsingScenario(playerPed, smokeScenario) then
+        ClearPedTasks(playerPed)
+        return
+    end
+
+    TaskStartScenarioInPlace(playerPed, smokeScenario, 0, true)
+end, false)
+
+-- Cigarette hash and entity id
+local cigHash = `ng_proc_cigarette01a`
+local cigObj = 0
+
+-- Smoke animation
+local SmokeAnimations = {
+    smoke = { name = 'base', dictionary = 'amb@world_human_smoking@male@male_a@base', flag = 49 },
+    exit = { name = 'exit', dictionary = 'amb@world_human_smoking@male@male_a@exit', flag = 48 },
+}
+
+-- Load all smoke animations
+function loadSmokeAnimations()
+    for _, value in pairs(SmokeAnimations) do
+        if not HasAnimDictLoaded(value.dictionary) then
+            requestAnimation(value.dictionary)
+        end
+    end
+end
+
+-- Unload all smoke animations
+function unloadSmokeAnimations()
+    for _, value in pairs(SmokeAnimations) do
+        RemoveAnimDict(value.dictionary)
+    end
+end
+
+-- Smoke effects
+local cigParticleAsset = 'scr_mp_cig'
+local cigParticleName = 'ent_anim_cig_smoke'
+local cigParticleName2 = 'ent_anim_cig_exhale_mth'
+local cigParticleHandle = 0
+
+local isSmoking = false
+
+RegisterCommand('smoke', function()
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+
+    -- Check if already smoking
+    if isSmoking then
+        -- Stop the smoking animation
+        StopEntityAnim(playerPed, SmokeAnimations.smoke.name, SmokeAnimations.smoke.dictionary, 0)
+
+        -- Stop the particle animations
+        StopParticleFxLooped(cigParticleHandle, false)
+
+        -- Remove the asset after use
+        RemoveNamedPtfxAsset(cigParticleAsset)
+
+        -- Do the exit smoke animation
+        TaskPlayAnim(playerPed, SmokeAnimations.exit.dictionary, SmokeAnimations.exit.name,
+            8.0, 8.0, -1, SmokeAnimations.exit.flag, 0.0, false, false, false)
+
+        -- Wait halfway through the animation before detaching
+        Wait(GetAnimDuration(SmokeAnimations.exit.dictionary, SmokeAnimations.exit.name) * 600)
+
+        -- Detach the cigarette
+        DetachEntity(cigObj, true, true)
+
+        -- Wait a second for cigarette to drop to the ground
+        Wait(1000)
+
+        -- Delete the cigarette object
+        DeleteEntity(cigObj)
+
+        -- Unload all smoking animations
+        unloadSmokeAnimations()
+
+        -- Reset the isSmoking property
+        isSmoking = false
+
+        return
+    end
+
+    -- Load the smoke animation
+    loadSmokeAnimations()
+
+    -- Load and create the cigarette object
+    loadModel(cigHash)
+    cigObj = CreateObject(cigHash, playerCoords.x, playerCoords.y, playerCoords.z, true,
+        true, false)
+    SetModelAsNoLongerNeeded(cigHash)
+
+    -- Attach the cigarette to the player's right index finger
+    AttachEntityToEntity(cigObj, playerPed, GetEntityBoneIndexByName(playerPed, 'BONETAG_R_FINGER11'),
+        0.0, 0.02, -0.01, 0.0, 0.0, 70.0,
+        true, true, false, false, 2, true)
+
+    -- Do the smoke animation
+    TaskPlayAnim(playerPed, SmokeAnimations.smoke.dictionary, SmokeAnimations.smoke.name,
+        8.0, 8.0, -1, SmokeAnimations.smoke.flag, 0.0, false, false, false)
+
+    -- Load the asset
+    RequestNamedPtfxAsset(cigParticleAsset)
+    repeat
+        Wait(0)
+    until HasNamedPtfxAssetLoaded(cigParticleAsset)
+
+    -- Use the asset
+    UseParticleFxAsset(cigParticleAsset)
+
+    -- Start the particle looped animation on the cigarette
+    cigParticleHandle = StartNetworkedParticleFxLoopedOnEntity(cigParticleName, cigObj,
+        -0.06, 0.0, 0.0, 0.0, 0.0, 70.0, 1.5,
+        true, true, true)
+
+    -- -- Start the particle looped animation on the ped's mouth
+    -- StartNetworkedParticleFxNonLoopedOnEntityBone(cigParticleName2, playerPed,
+    --     0.05, 0.0, 0.0, 0.0, 0.0, 0.0, GetPedBoneIndex(playerPed, 0xB987), 2.0,
+    --     true, true, true)
+
+    -- Set isSmoking to true
+    isSmoking = true
 end, false)
 
 --- ============================
